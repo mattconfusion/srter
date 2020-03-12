4a965e0c8c85c1e7338d11c87a9afc63a3c163a4
Matteo Radice <mattconfusion@hotmail.com>
2020-03-13 00:39:24 +0100

    tested collections and whatsit in sample di fle

--- di.php
+++ di.php
@@ -3,10 +3,20 @@
 namespace Srter;
 
 use Srter\FileManager\Folder;
+use Srter\FileManager\SrtCollection;
+use Srter\FileManager\SrtFile;
 use Srter\FileManager\SrtFileFinder;
 
 require 'vendor/autoload.php';
 
 $builder = new \DI\Container();
 $finder = new SrtFileFinder();
-var_export($finder->scanForFiles(new Folder($argv[1]))); 
+$srtFiles = $finder->scanForFiles(new Folder($argv[1]));
+var_export($srtFiles);
+
+$collection = new SrtCollection(...[]);
+\array_walk($srtFiles, function (string $filePath) use ($collection) {
+    $collection->addFile(new SrtFile($filePath));
+});
+
+var_export($collection);
